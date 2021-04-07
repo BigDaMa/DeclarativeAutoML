7b1424d9ce49fed555a8a952468b547c958b9375
Felix Neutatz <neutatz@gmail.com>
2021-04-07 11:38:14 +0200

    fix parallelism

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/check_model_parallel_per_data.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/check_model_parallel_per_data.py
@@ -98,7 +98,7 @@ for test_holdout_dataset_id in [args.dataset]:
                                                                                    #evaluation_time=int(0.1*search_time_frozen),
                                                                                    #hold_out_fraction=0.33,
                                                                                    tune_space=True,
-                                                                                   ), n_trials=500, n_jobs=8)
+                                                                                   ), n_trials=500, n_jobs=1)
 
             space = study_prune.best_trial.user_attrs['space']
 
