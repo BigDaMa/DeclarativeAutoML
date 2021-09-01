f57010ed34875545f987b0fed609c6c3e3bc5d79
Felix Neutatz <neutatz@gmail.com>
2021-09-01 11:47:07 +0200

    add package for smac

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ setup(
                       "libtmux",
                       "matplotlib",
                       "imbalanced-learn==0.8.0",
-                      "smac==0.13.1"
+                      "smac==0.13.1",
+                      "pyDOE==0.3.8"
                       ],
     packages=find_packages(exclude=('tests', 'docs'))
 )
