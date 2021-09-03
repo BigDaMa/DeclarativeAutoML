d62e5d524cd823119998a9b9b1c5c54e39f8ee14
Felix Neutatz <neutatz@gmail.com>
2021-09-03 13:34:31 +0200

    go to newest dask

--- setup.py
+++ setup.py
@@ -37,8 +37,6 @@ setup(
                       "pyDOE==0.3.8",
                       "emcee==3.0.2",
                       "scikit-optimize==0.8.1",
-                      "dask==1.2.2",
-                      "distributed=1.28.1"
                       ],
     packages=find_packages(exclude=('tests', 'docs'))
 )
