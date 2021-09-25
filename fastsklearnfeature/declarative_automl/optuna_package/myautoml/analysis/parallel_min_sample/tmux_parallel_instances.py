14bba232696e608926baf8d0af7c51b243aed6c0
Felix Neutatz <neutatz@gmail.com>
2021-09-25 10:59:51 +0200

    change name

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_min_sample/tmux_parallel_instances.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_min_sample/tmux_parallel_instances.py
@@ -8,7 +8,7 @@ datasets = [168794, 168797, 168796, 189871, 189861, 167185, 189872, 189908, 7510
 
 
 program = '/home/neutatz/Software/DeclarativeAutoML/fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_min_sample/check_model_parallel_per_data_minimum_sample_instances.py'
-outputname = 'sample_instances_new'
+outputname = 'sample_instances_new2'
 
 conda_name = 'AutoMLD'
 
