9a7749b07aaec9704b5f0457dfc0b61e68f11e05
Felix Neutatz <neutatz@gmail.com>
2021-11-01 11:26:54 +0100

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 python -m pip install .
-python -m pip install auto-sklearn==0.10.0
+#python -m pip install auto-sklearn==0.10.0
 python -m pip install openml==0.12.2
 python -m pip install scikit-learn==0.24.1
 python -m pip install pandas==0.25.3
