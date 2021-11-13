091752ccdcfc8b8073d7abbc9cde75f07ba68ad4
Felix Neutatz <neutatz@gmail.com>
2021-11-13 19:03:12 +0100

    fix bug

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_autosklearn/tmux_parallel_all.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_autosklearn/tmux_parallel_all.py
@@ -7,7 +7,7 @@ datasets = [168794, 168797, 168796, 189871, 189861, 167185, 189872, 189908, 7510
 
 
 
-program = '/home/neutatz/Software/DeclarativeAutoML/fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only/check_model_parallel_per_data_minimum_all1.py'
+program = '/home/neutatz/Software/DeclarativeAutoML/fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_autosklearn/check_model_parallel_per_data_minimum_all1.py'
 outputname = 'autosklearn_all'
 
 conda_name = 'AutoMLD'
