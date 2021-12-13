f7807bc621c97769be3e668207d7fb6157cf8093
Felix Neutatz <neutatz@gmail.com>
2021-12-13 02:42:42 +0100

    run random 1 week

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/AL_minimal_sample10_all_param_final_1min_rand163_iter100_random.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/AL_minimal_sample10_all_param_final_1min_rand163_iter100_random.py
@@ -334,7 +334,7 @@ def get_best_trial():
             return study_uncertainty.best_trial
 
 def sample_and_evaluate(my_id1):
-    if time.time() - starting_time_tt > 60*60*24:
+    if time.time() - starting_time_tt > 60*60*24*7:
         return -1
 
     try:
