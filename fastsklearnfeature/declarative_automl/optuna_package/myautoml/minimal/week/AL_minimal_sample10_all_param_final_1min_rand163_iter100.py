141fa1a4bcfecbd9902240119eec0967b5b526c0
Felix Neutatz <neutatz@gmail.com>
2021-12-21 16:23:59 +0100

    run 10min for 2 weeks

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/AL_minimal_sample10_all_param_final_1min_rand163_iter100.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/AL_minimal_sample10_all_param_final_1min_rand163_iter100.py
@@ -36,7 +36,7 @@ my_openml_tasks = [75126, 75125, 75121, 75120, 75116, 75115, 75114, 189859, 1898
 my_scorer = make_scorer(balanced_accuracy_score)
 
 
-mp_glob.total_search_time = 5*60#60
+mp_glob.total_search_time = 10*60#60
 topk = 26 # 20
 continue_from_checkpoint = False
 
@@ -343,7 +343,7 @@ def get_best_trial(model_uncertainty):
     return study_uncertainty.best_trial
 
 def sample_and_evaluate(my_id1):
-    if time.time() - starting_time_tt > 60*60*24*7:
+    if time.time() - starting_time_tt > 60*60*24*14:
         return -1
 
     X_meta = copy.deepcopy(dictionary['X_meta'])
