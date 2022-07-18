15eb8a3cce5071bf7c4c5f8d3b980652e6433538
Felix Neutatz <neutatz@gmail.com>
2022-07-18 15:50:29 +0200

    fix pool

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/experiments/find_constraint_space1_fairness.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/experiments/find_constraint_space1_fairness.py
@@ -26,6 +26,27 @@ from sklearn.metrics import balanced_accuracy_score
 import random
 from fastsklearnfeature.declarative_automl.fair_data.test import get_X_y_id
 import sklearn
+import multiprocessing
+
+class NoDaemonProcess(multiprocessing.Process):
+    @property
+    def daemon(self):
+        return False
+
+    @daemon.setter
+    def daemon(self, value):
+        pass
+
+
+class NoDaemonContext(type(multiprocessing.get_context())):
+    Process = NoDaemonProcess
+
+# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# because the latter is only a wrapper function, not a proper class.
+class NestablePool(multiprocessing.pool.Pool):
+    def __init__(self, *args, **kwargs):
+        kwargs['context'] = NoDaemonContext()
+        super(NestablePool, self).__init__(*args, **kwargs)
 
 openml.config.apikey = '4384bd56dad8c3d2c0f6630c52ef5567'
 openml.config.cache_directory = '/home/neutatz/phd2/cache_openml'
@@ -306,8 +327,8 @@ dictionary['inference_time'] = np.array([])
 dictionary['pipeline_size'] = np.array([])
 dictionary['fairness'] = np.array([])
 
-from multiprocessing import Pool
-with Pool(processes=topk) as pool:
+
+with NestablePool(processes=topk) as pool:
     results = pool.map(sample_and_evaluate, range(100000))
 
 print('storing stuff')
