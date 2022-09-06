2de8a885e5e06358097be59911f248ffc2501ee4
Felix Neutatz <neutatz@gmail.com>
2022-09-07 00:05:14 +0200

    fix paths

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_ensemble/check_model_parallel_per_data_minimum_all1.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_ensemble/check_model_parallel_per_data_minimum_all1.py
@@ -1,3 +1,5 @@
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
 from fastsklearnfeature.declarative_automl.optuna_package.myautoml.my_system.ensemble.AutoEnsembleSuccessive import \
     MyAutoML as AutoEnsembleML
 
@@ -18,9 +20,10 @@ import fastsklearnfeature.declarative_automl.optuna_package.myautoml.analysis.pa
 from sklearn.metrics import balanced_accuracy_score
 import time
 from optuna.samplers import NSGAIISampler
+import getpass
 
 openml.config.apikey = '4384bd56dad8c3d2c0f6630c52ef5567'
-openml.config.cache_directory = '/home/neutatz/phd2/cache_openml'
+openml.config.cache_directory = '/home/' + getpass.getuser() + '/phd2/cache_openml'
 
 my_scorer = make_scorer(balanced_accuracy_score)
 
@@ -46,12 +49,7 @@ for test_holdout_dataset_id in [args.dataset]:
     X_train_hold, X_test_hold, y_train_hold, y_test_hold, categorical_indicator_hold, attribute_names_hold = get_data('data', randomstate=42, task_id=test_holdout_dataset_id)
     metafeature_values_hold = data2features(X_train_hold, y_train_hold, categorical_indicator_hold)
 
-    #model_success = pickle.load(open('/home/neutatz/phd2/decAutoML2weeks_compare2default/training_sampling_min_2Drandom_machine2/my_great_model_compare_scaled.p', "rb"))
-    #model_success = pickle.load(open('/home/neutatz/phd2/decAutoML2weeks_compare2default/july30_machine1/my_great_model_compare_scaled.p', "rb"))
-    #model_success = pickle.load(open('/home/neutatz/phd2/decAutoML2weeks_compare2default/july30_machine4/my_great_model_compare_scaled.p', "rb"))
-    model_success = pickle.load(open('/home/neutatz/data/my_temp/my_great_model_compare_scaled.p', "rb"))
-    #model_success = pickle.load(open('/home/neutatz/phd2/decAutoML2weeks_compare2default/sep14_sampling/my_great_model_compare_scaled.p', "rb"))
-    #model_success = pickle.load(open('/home/neutatz/phd2/decAutoML2weeks_compare2default/model_1_week_dez_17/my_great_model_compare_scaled.p', "rb"))
+    model_success = pickle.load(open('/home/' + getpass.getuser() + '/data/my_temp/my_great_model_compare_scaled.p', "rb"))
 
     my_list_constraints = ['global_search_time_constraint',
                            'global_evaluation_time_constraint',
@@ -159,9 +157,9 @@ for test_holdout_dataset_id in [args.dataset]:
     results_dict_log = {}
     results_dict_log['dynamic'] = new_constraint_evaluation_dynamic_all
 
-    pickle.dump(results_dict_log, open('/home/neutatz/data/automl_runs/log_' + args.outputname + '_' + str(test_holdout_dataset_id) + '.p', 'wb+'))
+    pickle.dump(results_dict_log, open('/home/' + getpass.getuser() + '/data/automl_runs/log_' + args.outputname + '_' + str(test_holdout_dataset_id) + '.p', 'wb+'))
 
     results_dict = {}
     results_dict['dynamic'] = dynamic_approach
 
-    pickle.dump(results_dict, open('/home/neutatz/data/automl_runs/' + args.outputname + '_' + str(test_holdout_dataset_id) + '.p', 'wb+'))
+    pickle.dump(results_dict, open('/home/' + getpass.getuser() + '/data/automl_runs/' + args.outputname + '_' + str(test_holdout_dataset_id) + '.p', 'wb+'))
