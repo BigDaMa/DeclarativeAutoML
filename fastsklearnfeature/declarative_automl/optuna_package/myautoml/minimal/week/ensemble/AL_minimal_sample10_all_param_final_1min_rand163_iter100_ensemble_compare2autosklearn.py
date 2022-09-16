4cb0bdd05235cb72e72c3e8f89334413fbfcc824
Felix Neutatz <neutatz@gmail.com>
2022-09-16 14:44:07 +0200

    run 1 day autosklearn2

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/ensemble/AL_minimal_sample10_all_param_final_1min_rand163_iter100_ensemble_compare2autosklearn.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/ensemble/AL_minimal_sample10_all_param_final_1min_rand163_iter100_ensemble_compare2autosklearn.py
@@ -386,7 +386,7 @@ def get_best_trial(model_uncertainty):
     return study_uncertainty.best_trial
 
 def sample_and_evaluate(my_id1):
-    if time.time() - starting_time_tt > 60*60*24*7:
+    if time.time() - starting_time_tt > 60*60*24*1:
         return -1
 
     X_meta = copy.deepcopy(dictionary['X_meta'])
