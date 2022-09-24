dec7b388df3f27d1412e3a205bad67f61b12474b
Felix Neutatz <neutatz@gmail.com>
2022-09-24 23:16:54 +0200

    fix path

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_constraints/tmux_parallel_all.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_constraints/tmux_parallel_all.py
@@ -2,12 +2,13 @@ import libtmux
 from pathlib import Path
 import time
 import argparse
+import getpass
 
 datasets = [168794, 168797, 168796, 189871, 189861, 167185, 189872, 189908, 75105, 167152, 168793, 189860, 189862, 126026, 189866, 189873, 168792, 75193, 168798, 167201, 167149, 167200, 189874, 167181, 167083, 167161, 189865, 189906, 167168, 126029, 167104, 126025, 75097, 168795, 75127, 189905, 189909, 167190, 167184]
 
 
 
-program = '/home/neutatz/Software/DeclarativeAutoML/fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_constraints/check_model_parallel_per_data_minimum_all1.py'
+program = '/home/' + getpass.getuser() + '/Software/DeclarativeAutoML/fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_constraints/check_model_parallel_per_data_minimum_all1.py'
 outputname = 'sample_instances_new2_all_constraints'
 
 conda_name = 'AutoMLD'
@@ -22,7 +23,7 @@ finished = []
 session = server.new_session(session_name="install", kill_session=True, attach=False)
 session.attached_pane.send_keys('exec bash')
 session.attached_pane.send_keys('conda activate ' + conda_name)
-session.attached_pane.send_keys('cd /home/neutatz/Software/DeclarativeAutoML')
+session.attached_pane.send_keys('cd /home/' + getpass.getuser() + '/Software/DeclarativeAutoML')
 #session.attached_pane.send_keys('git pull origin main')
 #session.attached_pane.send_keys('python -m pip install .')
 
@@ -36,7 +37,7 @@ while len(finished) < len(datasets):
         running_ids.append(datasets[data_id])
         session.attached_pane.send_keys('exec bash')
         session.attached_pane.send_keys('conda activate ' + conda_name)
-        session.attached_pane.send_keys('cd /home/neutatz/Software/DeclarativeAutoML')
+        session.attached_pane.send_keys('cd /home/' + getpass.getuser() + '/Software/DeclarativeAutoML')
         session.attached_pane.send_keys('python ' + program + ' -d ' + str(datasets[data_id]) + ' -o ' + str(outputname))
         data_id += 1
 
@@ -44,7 +45,7 @@ while len(finished) < len(datasets):
     #check if anything is done
     to_be_removed = []
     for r in running_ids:
-        my_file = Path('/home/neutatz/data/automl_runs/' + outputname + '_' + str(r) + '.p')
+        my_file = Path('/home/' + getpass.getuser() + '/data/automl_runs/' + outputname + '_' + str(r) + '.p')
         if my_file.is_file():
             time.sleep(60)
             session = server.find_where({"session_name": "data" + str(r)})
