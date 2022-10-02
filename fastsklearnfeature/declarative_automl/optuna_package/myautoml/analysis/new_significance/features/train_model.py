4d461c2c22e0021a4e5b44a6331f869f81b8f57f
Felix Neutatz <neutatz@gmail.com>
2022-10-03 00:52:58 +0200

    run longer

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/new_significance/features/train_model.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/new_significance/features/train_model.py
@@ -70,7 +70,7 @@ def objective(trial):
 
 #study = optuna.create_study(direction='maximize')
 study = optuna.create_study(direction='minimize')
-study.optimize(objective, n_trials=100)
+study.optimize(objective, n_trials=1000)
 
 print(study.best_params)
 
