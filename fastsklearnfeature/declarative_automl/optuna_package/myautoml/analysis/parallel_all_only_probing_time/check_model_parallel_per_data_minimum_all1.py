56282f4379f0912b202ac9fdbb7cba7e72a1de78
Felix Neutatz <neutatz@gmail.com>
2022-10-07 17:13:25 +0200

    check probing time

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_probing_time/check_model_parallel_per_data_minimum_all1.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_probing_time/check_model_parallel_per_data_minimum_all1.py
@@ -1,12 +1,10 @@
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.my_system.MyAutoMLProcessClassBalanceDict import MyAutoML
 import optuna
 from sklearn.metrics import make_scorer
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.my_system.Space_GenerationTreeBalance import SpaceGenerator
 import pickle
 from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import get_data
 from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import data2features
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import optimize_accuracy_under_minimal_sample
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import utils_run_AutoML
+from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import optimize_accuracy_under_minimal_sample_ensemble
+from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import utils_run_AutoML_ensemble
 from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import get_feature_names
 from fastsklearnfeature.declarative_automl.optuna_package.myautoml.analysis.parallel.util_classes_new import ConstraintEvaluation, ConstraintRun, space2str
 from anytree import RenderTree
@@ -14,8 +12,8 @@ import argparse
 import openml
 import fastsklearnfeature.declarative_automl.optuna_package.myautoml.analysis.parallel.my_global_vars as mp_global
 from sklearn.metrics import balanced_accuracy_score
-from sklearn.ensemble import RandomForestRegressor
 from optuna.samplers import NSGAIISampler
+import getpass
 
 openml.config.apikey = '4384bd56dad8c3d2c0f6630c52ef5567'
 openml.config.cache_directory = '/home/neutatz/phd2/cache_openml'
@@ -44,14 +42,8 @@ for test_holdout_dataset_id in [args.dataset]:
     X_train_hold, X_test_hold, y_train_hold, y_test_hold, categorical_indicator_hold, attribute_names_hold = get_data('data', randomstate=42, task_id=test_holdout_dataset_id)
     metafeature_values_hold = data2features(X_train_hold, y_train_hold, categorical_indicator_hold)
 
-    #model_success = pickle.load(open('/home/neutatz/phd2/decAutoML2weeks_compare2default/training_sampling_min_2Drandom_machine2/my_great_model_compare_scaled.p', "rb"))
-    #model_success = pickle.load(open('/home/neutatz/phd2/decAutoML2weeks_compare2default/july30_machine1/my_great_model_compare_scaled.p', "rb"))
-    #model_success = pickle.load(open('/home/neutatz/phd2/decAutoML2weeks_compare2default/july30_machine4/my_great_model_compare_scaled.p', "rb"))
-    #model_success = pickle.load(open('/home/neutatz/data/model_no_constraints/my_great_model_compare_scaled.p', "rb"))
-    #model_success = pickle.load(open('/home/neutatz/phd2/decAutoML2weeks_compare2default/sep14_sampling/my_great_model_compare_scaled.p', "rb"))
-
-    X_surrogate = pickle.load(open('/home/neutatz/data/my_temp/felix_X_compare_scaled.p', "rb"))
-    y_surrogate = pickle.load(open('/home/neutatz/data/my_temp/felix_y_compare_scaled.p', "rb"))
+    model_success = pickle.load(
+        open('/home/' + getpass.getuser() + '/data/my_temp/my_great_model_compare_scaled.p', "rb"))
 
     my_list_constraints = ['global_search_time_constraint',
                            'global_evaluation_time_constraint',
@@ -75,10 +67,6 @@ for test_holdout_dataset_id in [args.dataset]:
 
     for probing_time in [1, 5, 10, 30, 60]:#[1, 5, 10, 60]:#range(1, 6):
 
-        days_surrogate = 7
-        model_success = RandomForestRegressor(n_estimators=1000, random_state=42, n_jobs=1)
-        model_success.fit(X_surrogate[0: int((days_surrogate / 7.0) * len(X_surrogate)), :], y_surrogate[0: int((days_surrogate / 7.0) * len(X_surrogate))])
-
         current_dynamic = []
 
         minutes_to_search = 5
@@ -89,8 +77,9 @@ for test_holdout_dataset_id in [args.dataset]:
                                                                  system_def='dynamic')
         for repeat in range(10):
 
-            mp_global.study_prune = optuna.create_study(direction='maximize', sampler=NSGAIISampler())
-            mp_global.study_prune.optimize(lambda trial: optimize_accuracy_under_minimal_sample(trial=trial,
+            #mp_global.study_prune = optuna.create_study(direction='maximize', sampler=NSGAIISampler())
+            mp_global.study_prune = optuna.create_study(direction='maximize')
+            mp_global.study_prune.optimize(lambda trial: optimize_accuracy_under_minimal_sample_ensemble(trial=trial,
                                                                                    metafeature_values_hold=metafeature_values_hold,
                                                                                    search_time=search_time_frozen,
                                                                                    model_success=model_success,
@@ -98,7 +87,8 @@ for test_holdout_dataset_id in [args.dataset]:
                                                                                    privacy_limit=privacy,
                                                                                    #evaluation_time=int(0.1*search_time_frozen),
                                                                                    #hold_out_fraction=0.33,
-                                                                                   tune_space=True
+                                                                                   tune_space=True,
+                                                                                   tune_val_fraction=True
                                                                                    ), timeout=probing_time, n_jobs=1)
 
             space = mp_global.study_prune.best_trial.user_attrs['space']
@@ -110,33 +100,18 @@ for test_holdout_dataset_id in [args.dataset]:
             try:
                 result = None
                 search_dynamic = None
-                if mp_global.study_prune.best_trial.value > 0.5:
-                    result, search_dynamic = utils_run_AutoML(mp_global.study_prune.best_trial,
-                                                                 X_train=X_train_hold,
-                                                                 X_test=X_test_hold,
-                                                                 y_train=y_train_hold,
-                                                                 y_test=y_test_hold,
-                                                                 categorical_indicator=categorical_indicator_hold,
-                                                                 my_scorer=my_scorer,
-                                                                 search_time=search_time_frozen,
-                                                                 memory_limit=memory_budget,
-                                                                 privacy_limit=privacy
-                                                 )
-                else:
-                    gen_new = SpaceGenerator()
-                    space = gen_new.generate_params()
 
-                    search_default = MyAutoML(n_jobs=1,
-                                              time_search_budget=search_time_frozen,
-                                              space=space,
-                                              evaluation_budget=int(0.1 * search_time_frozen),
-                                              main_memory_budget_gb=memory_budget,
-                                              differential_privacy_epsilon=privacy,
-                                              hold_out_fraction=0.33
-                                              )
-
-                    best_result = search_default.fit(X_train_hold, y_train_hold, categorical_indicator=categorical_indicator_hold, scorer=my_scorer)
-                    result = my_scorer(search_default.get_best_pipeline(), X_test_hold, y_test_hold)
+                result, search_dynamic = utils_run_AutoML_ensemble(mp_global.study_prune.best_trial,
+                                                             X_train=X_train_hold,
+                                                             X_test=X_test_hold,
+                                                             y_train=y_train_hold,
+                                                             y_test=y_test_hold,
+                                                             categorical_indicator=categorical_indicator_hold,
+                                                             my_scorer=my_scorer,
+                                                             search_time=search_time_frozen,
+                                                             memory_limit=memory_budget,
+                                                             privacy_limit=privacy
+                                             )
 
                 new_constraint_evaluation_dynamic.append(ConstraintRun(space_str=space2str(space.parameter_tree), params=mp_global.study_prune.best_trial.params, test_score=result, estimated_score=mp_global.study_prune.best_trial.value))
             except:
