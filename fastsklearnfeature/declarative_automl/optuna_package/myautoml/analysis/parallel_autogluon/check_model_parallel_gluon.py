6781cf3b6da4a09944d009eed4aa549eb0c9ece9
Felix Neutatz <neutatz@gmail.com>
2022-10-10 02:06:44 +0200

    run full autogluon again

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_autogluon/check_model_parallel_gluon.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_autogluon/check_model_parallel_gluon.py
@@ -39,7 +39,7 @@ for test_holdout_dataset_id in [args.dataset]:
 
     new_constraint_evaluation_dynamic_all = []
 
-    for minutes_to_search in [10, 30]:
+    for minutes_to_search in [10, 30, 60, 5*60, 10*60, 60*60]:
 
         current_dynamic = []
         search_time_frozen = minutes_to_search #* 60
