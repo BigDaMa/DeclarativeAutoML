ad107acabe61f2cc1bdd4174e4bd5dacd904ebe7
Felix Neutatz <neutatz@googlemail.com>
2022-10-15 15:01:33 +0200

    1 week

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/constraints/run_constraints_all_random.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/constraints/run_constraints_all_random.py
@@ -476,7 +476,7 @@ def get_best_trial():
     return study_uncertainty.best_trial
 
 def sample_and_evaluate(my_id1):
-    if time.time() - starting_time_tt > 60*60*24*1:
+    if time.time() - starting_time_tt > 60*60*24*7:
         return -1
 
     X_meta = copy.deepcopy(dictionary['X_meta'])
