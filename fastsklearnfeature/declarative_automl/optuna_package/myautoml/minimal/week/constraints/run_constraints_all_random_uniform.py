6e8ee9c52416fe031e318b1fc39076c84fa4468d
Felix Neutatz <neutatz@googlemail.com>
2022-11-29 00:12:03 +0100

    run for 10 mins

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/constraints/run_constraints_all_random_uniform.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/constraints/run_constraints_all_random_uniform.py
@@ -83,7 +83,7 @@ my_openml_tasks_fair = list(map_dataset.keys())
 my_scorer = make_scorer(balanced_accuracy_score)
 
 
-mp_glob.total_search_time = 5*60#60
+mp_glob.total_search_time = 10*60#60
 topk = 20#26 # 20
 continue_from_checkpoint = False
 
@@ -471,7 +471,7 @@ def get_best_trial():
     return study_uncertainty.best_trial
 
 def sample_and_evaluate(my_id1):
-    if time.time() - starting_time_tt > 60*60*24*14:
+    if time.time() - starting_time_tt > 60*60*24*7:
         return -1
 
     X_meta = copy.deepcopy(dictionary['X_meta'])
