2a7cf519a803ee309a5e5428c34063d0e8611b82
Felix Neutatz <neutatz@googlemail.com>
2023-01-04 10:44:54 +0100

    hybrid=0.4

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_ensemble_random_classification_searchtime_hyprid/check_model_parallel_per_data_minimum_all1.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_ensemble_random_classification_searchtime_hyprid/check_model_parallel_per_data_minimum_all1.py
@@ -161,8 +161,12 @@ for test_holdout_dataset_id in [args.dataset]:
                 predictions += predictions_curr
 
             best_id = np.argmax(predictions)
-            if predictions[predictions_regression_best_id] > 0.5:
+            if predictions[predictions_regression_best_id] > 0.4:
                 best_id = predictions_regression_best_id
+                print('regression model')
+            else:
+                print('classification model')
+
 
             print('best_id: ' + str(best_id))
             print('max:' + str(np.max(predictions)))
