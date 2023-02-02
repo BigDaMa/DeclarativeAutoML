f3cb7fd7589c7c94ede4a09f386a21096ec4e7e1
Felix Neutatz <neutatz@googlemail.com>
2023-02-02 11:24:57 +0100

    change autosklearn

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_autosklearn2_new/check_model_parallel.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_autosklearn2_new/check_model_parallel.py
@@ -39,7 +39,7 @@ for test_holdout_dataset_id in [args.dataset]:
 
     new_constraint_evaluation_dynamic_all = []
 
-    for minutes_to_search in [1, 5, 10, 60]:
+    for minutes_to_search in [5]:
 
         current_dynamic = []
         search_time_frozen = minutes_to_search * 60
@@ -50,7 +50,7 @@ for test_holdout_dataset_id in [args.dataset]:
         for repeat in range(10):
 
             tmp_path = "/home/" + getpass.getuser() + "/data/auto_tmp/autosklearn" + str(time.time()) + '_' + str(
-                np.random.randint(1000) + 'folder')
+                np.random.randint(1000)) + 'folder'
 
             try:
                 tracker = EmissionsTracker()
