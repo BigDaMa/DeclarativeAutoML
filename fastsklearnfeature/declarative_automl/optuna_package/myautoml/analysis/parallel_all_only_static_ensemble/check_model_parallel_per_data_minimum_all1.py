d197ed0191f05972c6c23265b5dbe983a2fe0aa4
Felix Neutatz <neutatz@googlemail.com>
2023-07-11 11:10:36 +0200

    run

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_static_ensemble/check_model_parallel_per_data_minimum_all1.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_static_ensemble/check_model_parallel_per_data_minimum_all1.py
@@ -69,7 +69,7 @@ for test_holdout_dataset_id in [args.dataset]:
 
     new_constraint_evaluation_dynamic_all = []
 
-    for minutes_to_search in [10, 30, 60, 5*60, 10*60, 60*60]:#[1, 5, 10, 60]:#range(1, 6):
+    for minutes_to_search in [30*60, 60*60]:#[1, 5, 10, 60]:#range(1, 6):
 
         current_dynamic = []
 
