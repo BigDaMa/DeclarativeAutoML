fa10ac37103ae58e1ba40a80b79c28bfad82dadc
Felix Neutatz <neutatz@googlemail.com>
2023-07-24 14:28:55 +0200

    check

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_tpot/test2.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_tpot/test2.py
@@ -6,8 +6,7 @@ digits = load_digits()
 X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target,
                                                     train_size=0.75, test_size=0.25)
 
-pipeline_optimizer = TPOTClassifier(generations=1, population_size=20, cv=5,
-                                    random_state=42, verbosity=2)
+pipeline_optimizer = TPOTClassifier(random_state=42, verbosity=2, max_time_mins=2, scoring='balanced_accuracy', n_jobs=1)
 pipeline_optimizer.fit(X_train, y_train)
 print(pipeline_optimizer.score(X_test, y_test))
 pipeline_optimizer.export('tpot_exported_pipeline.py')
